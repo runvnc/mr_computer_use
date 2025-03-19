caba8a41647b1de939829c7f5fde838b61c150cf
Jason Livesay <runvnc@gmail.com>
2025-03-18 23:14:29 -0500

    updates

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="mr_computer_use",
-    version="1.0.2",
+    version="1.1.0",
     packages=find_packages(where="src"),
     package_dir={"":"src"},
     package_data={
         "mr_computer_use": [
             "static/js/*.js",
             "templates/*.jinja2",
-            "inject/*.jinja2"
+            "inject/*.jinja2",
+            "override/*.jinja2"
         ],
     },
     install_requires=[
