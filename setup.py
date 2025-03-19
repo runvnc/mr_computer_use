406165c6cbf64a3d730bde0e5886048704318e72
Jason Livesay <runvnc@gmail.com>
2025-03-18 20:03:05 -0500

    updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mr_computer_use",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(where="src"),
     package_dir={"":"src"},
     package_data={
